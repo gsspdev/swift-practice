e0326649bd065931dc260fbae02eb525d7b43f7c
gsspdev <dev@gssp.io>
2023-09-01 08:06:34 -0700

    more switches and enums practice

--- swift-structs-and-synths.playground/Contents.swift
+++ swift-structs-and-synths.playground/Contents.swift
@@ -55,3 +55,109 @@ arp2600.printSynthDetails()
 moog.printSynthDetails()
 dx7.printSynthDetails()
 hydrasynth.printSynthDetails()
+
+
+
+
+//enum Planet {
+//    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+//}
+
+//enum CompassPoint {
+//    case north
+//    case south
+//    case east
+//    case west
+//}
+
+var directionToHead = CompassPoint.east
+directionToHead = .north
+
+directionToHead = .west
+switch directionToHead {
+    case .north:
+        print("Ah to the mountains of Vindor")
+    case .south:
+        print("Ro the swamps of Shoggoroth")
+    case .west:
+        print("Across the hills and valleys of Montehey")
+    case .east:
+        print("Through the funneling forests of Elroth")
+}
+
+let somePlanet = Planet.earth
+switch somePlanet {
+case .earth:
+    print("great for humans")
+default:
+    print("not much explored by humans")
+}
+
+enum Beverage: CaseIterable {
+    case coffee, tea, juice
+}
+let numberOfChoices = Beverage.allCases.count
+print("\(numberOfChoices) beverages available")
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 84950, 38419, 2)
+productBarcode = .qrCode("ABDFKLJSNJK")
+
+switch productBarcode {
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+case .qrCode(let productCode):
+    print("QR CODE: \(productCode).")
+}
+
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+let earthsOrder = Planet.earth.rawValue
+
+enum CompassPoint: String {
+    case north, south, east, west
+}
+
+let sunsetDirection = CompassPoint.west.rawValue
+
+
+
+
+//enum ArithmeticExpression {
+//    case number(Int)
+//    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+//    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
+//}
+
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+
+print(evaluate(product))
+// Prints "18"
